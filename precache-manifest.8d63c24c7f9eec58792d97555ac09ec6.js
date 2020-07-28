e639ac0ee96bc38738bf1142b5ebc87b1be070a7
3rfanian <hooman.erfan@mail.polimi.it>
2020-07-28 18:13:38 +0430

    Updates

---
+++ precache-manifest.8d63c24c7f9eec58792d97555ac09ec6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81f568cb537496157652091b236dbaa3",
+    "url": "/react-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "538383bec851708b2d8b",
+    "url": "/react-tic-tac-toe/static/css/main.7201c35b.chunk.css"
+  },
+  {
+    "revision": "7de1f036a2157dac2058",
+    "url": "/react-tic-tac-toe/static/js/2.57343eaf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tic-tac-toe/static/js/2.57343eaf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "538383bec851708b2d8b",
+    "url": "/react-tic-tac-toe/static/js/main.b5625356.chunk.js"
+  },
+  {
+    "revision": "929d476f7b1ee9149c3a",
+    "url": "/react-tic-tac-toe/static/js/runtime-main.db7cfeb9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-tic-tac-toe/static/media/logo.5d5d9eef.svg"
+  }
+]);
